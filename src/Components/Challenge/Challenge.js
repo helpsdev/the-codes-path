41931a5aea4fa59d6c9692ce284947f5e89c8aaf
Edwin Aragon <ared2728@gmail.com>
2021-02-07 12:12:05 -0700

    Add AceEditor component

--- src/Components/Challenge/Challenge.js
+++ src/Components/Challenge/Challenge.js
@@ -1,15 +1,24 @@
 import './Challenge.css';
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import LessonsArray from "../../lessons.json";
+import AceEditor from "react-ace";
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/theme-monokai';
 
 function Challenge() {
     const { lessonId } = useParams();
     const currentLesson = getLessonByLessonId(lessonId);
+    const [aceEditorValue, setAceEditorValue] = useState(null);
+    
     return (
         <div className="challenge">
             <section className="code-arena">
-                <section className="coding-area"></section>
-                <section className="console">This is test</section>
+                <AceEditor className="coding-area" mode="javascript" 
+                    theme="monokai" width="100%" 
+                    fontSize={14} onChange={setAceEditorValue}
+                />
+                <section className="console">{aceEditorValue}</section>
             </section>
             <section className="explanation">
                 <p>{currentLesson.explanation}</p>
