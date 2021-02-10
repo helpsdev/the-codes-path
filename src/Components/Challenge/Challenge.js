84b6e1cf7b08e17e8c44bc2c8c4ac39c745615ed
Edwin Aragon <ared2728@gmail.com>
2021-02-10 08:30:02 -0700

    Add Prismjs to highlight code examples. Replace tokens in lesson's description to give human readable style

--- src/Components/Challenge/Challenge.js
+++ src/Components/Challenge/Challenge.js
@@ -5,10 +5,10 @@ import LessonsArray from "../../lessons.json";
 import AceEditor from "react-ace";
 import 'ace-builds/src-noconflict/mode-javascript';
 import 'ace-builds/src-noconflict/theme-monokai';
+import Prismjs from "prismjs";
 
 function Challenge() {
     const { lessonId } = useParams();
-    const currentLesson = getLessonByLessonId(lessonId);
     const [aceEditorValue, setAceEditorValue] = useState(null);
     
     return (
@@ -21,7 +21,7 @@ function Challenge() {
                 <section className="console" data-console></section>
             </section>
             <section className="explanation">
-                <p>{currentLesson.explanation}</p>
+                {getLessonExplanation(lessonId)}
                 <button type="button" onClick={runTests.bind(this, aceEditorValue)}>Run Tests</button>
             </section>
         </div>
@@ -36,6 +36,29 @@ function runTests(currentCode){
 }
 function getLessonByLessonId(lessonId) {
     return LessonsArray.find(l => l.id === Number(lessonId));
+}
+
+function getLessonExplanation(lessonId){
+    const { name, explanation: { description, examples } } = getLessonByLessonId(lessonId);
+
+    return (
+        <div>
+            <h1>{name.toUpperCase()}</h1>
+            {description.split("[BR]").map((p, index) => {
+                
+                let result = p;
+                for (const key in examples) {
+                    if (Object.hasOwnProperty.call(examples, key)) {
+                        // const exampleHighlighted = Prismjs.highlight("var myObject = { key1 : value1 }", Prism.languages.javascript, 'javascript');
+                        // result = p.replace(new RegExp(`\[${key}\]`, "g"), examples[key]);
+                    }
+                }
+
+                return <p key={index.toString()}>{result}</p>;
+            })}
+        </div>
+    );
+    
 }
 
 export default Challenge;
